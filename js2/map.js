c99ee9d8c70663ad57ae06071f43574ea9eedaf5
pzr <2604011581@qq.com>
2021-02-03 23:38:32 +0800

    log

--- js2/map.js
+++ js2/map.js
@@ -311,7 +311,7 @@ $(function () {
         legend: {
             orient: 'vertical',
             bottom: '5%',
-            right: '10%',
+            right: '18%',
             data: ['全部','100-25', '25-15', '15-10',"10-0"],
             textStyle: {
                 color: '#fff'
